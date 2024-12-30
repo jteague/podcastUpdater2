aac50bad274136e104152c1f6488b7b1bce2549b
Jeremiah Teague <jeremiahteague@gmail.com>
2024-12-30 14:14:12 -0700

    Added Episode user control

---
+++ screens/Episode.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PodcastUpdater2.screens;
+
+public partial class Episode : UserControl
+{
+    public Episode()
+    {
+        InitializeComponent();
+    }
+}
